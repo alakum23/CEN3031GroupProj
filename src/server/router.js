b6edd0abe5800ce8a61440441adce778d0ad0743
MuhammadEbrahim2361 <ebrahim.mahmed@ufl.edu>
2022-11-03 11:49:25 -0400

    Got live event data from nasa

--- src/server/router.js
+++ src/server/router.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+require('dotenv').config();
 
 // Creating express router
 const router = express.Router();
@@ -13,6 +15,18 @@ router.get('/viewer', (req, res) =>  {
 //Specify api routes (server routes that don't server HTML files but still return information)
 router.get('/api/test', (req, res) =>  {
     res.send({body: "Hello"});
+});
+
+router.get('/NASA/env', async (req, res) => {
+    const response = await fetch(`https://eonet.gsfc.nasa.gov/api/v3/events?api_key=` + process.env.NASA_API_KEY, {
+        method: "GET", 
+        json: true, 
+        status: 'open'
+    });
+
+    const data = await response.json();
+    console.log(data);
+    res.send(data);
 });
 
 //Specify the default webpage last!
