721559644da2094718f9550a6c734653d6be68c1
Sn00pyW00dst0ck <gabealdous@gmail.com>
2022-11-05 21:18:48 -0400

    Adding a devtool to the test build

--- webpack/webpack.tests.js
+++ webpack/webpack.tests.js
@@ -44,10 +44,6 @@ const getEntries = (folder) =>
 const testDirectory = path.resolve(__dirname, '../test/client/');
 const entries = getEntries(testDirectory);
 
-const finalEntry = {
-  "testBundle.test": Object.values(entries)
-};
-
 module.exports = env => {
 	// Exporting a function so we can determine the mode to build tests in from command line arguments
 	const webPack = env.mode === "production" ? require('./webpack.prod.js') : require('./webpack.dev.js');
@@ -55,7 +51,8 @@ module.exports = env => {
 	// Merge the common webpack config with the new one 
 	// Using Object.assign instead of merge because I didn't know about merge when I wrote this and this works fine
 	return (Object.assign({}, webPack, {
-		entry: finalEntry, 
+		entry: entries, 
+    devtool: 'source-map',
 		output: { 
 			filename: "[name].js", 
 			path: path.resolve(__dirname, '../dist/__tests__/'),
