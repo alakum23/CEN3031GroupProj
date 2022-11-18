90e24a58c71f7f6b1e5a440d0ec8befb0e3a0328
Sn00pyW00dst0ck <gabealdous@gmail.com>
2022-11-18 16:16:44 -0500

    Fixing flyTo clip into ground error

--- src/client/viewer.js
+++ src/client/viewer.js
@@ -9,7 +9,7 @@ if (module.hot) {
 }
 
 // Import some Cesium assets (functions, classes, etc)
-import { Ion, Viewer, createWorldTerrain, createOsmBuildings, Cartesian3, Math, Cartographic, ScreenSpaceEventType, BillboardCollection, EntityCollection, HeadingPitchRange } from "cesium";
+import { Ion, Viewer, createWorldTerrain, Color, createOsmBuildings, Cartesian3, Math, Cartographic, ScreenSpaceEventType, BillboardCollection, EntityCollection, HeadingPitchRange, sampleTerrainMostDetailed, Transforms, Entity} from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import Billboard from "cesium/Source/Scene/Billboard";
 
@@ -59,19 +59,30 @@ req.then(value =>  value.json().then(data =>  {
 
 
 // Setup mouse click action to make things in viewer clickable
-viewer.screenSpaceEventHandler.setInputAction(function onLeftClick(movement)  {
-	const pickedFeature = viewer.scene.pick(movement.position);
-	
+viewer.screenSpaceEventHandler.setInputAction(async function onLeftClick(movement)  {
 	// Anything from Cesium left clicking that we want to happen we can put here...
-	if (pickedFeature /*&& pickedFeature.collection === disasterPinsCollection*/)  {
-		// Below object has lat lon position (in radians)
-		// x: longitude, y: latitude, z: height
-		const positionCartographic = Cartographic.fromCartesian(pickedFeature.primitive.position);
-		
-		//console.log(positionCartographic);
 
-		console.log(pickedFeature);
-    viewer.flyTo(pickedFeature.id).then(() =>  viewer.flyTo(pickedFeature.id)); 
+	const pickedFeature = viewer.scene.pick(movement.position);	
+	if (pickedFeature)  {
+		// Get the terrain based location of the entity we picked 
+		const cartographicPos = Cartographic.fromCartesian(pickedFeature.primitive.position);
+		const terrainLocation = await sampleTerrainMostDetailed(viewer.terrainProvider, [cartographicPos]);
+
+		// Create a temporary entity to fly to (needed to prevent clipping into ground during flight)
+		const tempEntity = viewer.entities.add({
+			position: Cartesian3.fromRadians(terrainLocation[0].longitude, terrainLocation[0].latitude, terrainLocation[0].height),
+			point : {
+				pixelSize : 0,
+				color : Color.TRANSPARENT,
+				outlineColor : Color.TRANSPARENT,
+				outlineWidth : 0
+			},
+		});
+
+		// Fly to that temporary entity and then delete it
+		viewer.flyTo(tempEntity, { 
+			offset: new HeadingPitchRange(undefined, -Math.PI/6, 1000) 
+		}).then(() => viewer.entities.remove(tempEntity));
 	}
 
 }, ScreenSpaceEventType.LEFT_CLICK);
