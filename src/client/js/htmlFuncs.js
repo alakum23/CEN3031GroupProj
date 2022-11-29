bd0c34a1666afb0e65641c8845207317960cb7a8
Sn00pyW00dst0ck <gabealdous@gmail.com>
2022-11-29 18:30:19 -0500

    Got date search working

--- src/client/js/htmlFuncs.js
+++ src/client/js/htmlFuncs.js
@@ -6,18 +6,32 @@ window.buttonClicked = function()  {
     console.log("A button was clicked");
 }
 
-window.getFromSearch = function()  {
-    var dateInput = document.getElementById('search-date').value;
-    var locationInput = document.getElementById('search-location').value;
-    var disasterInput = document.getElementById('search-disaster').value;
+window.getFromSearch = async function()  {
+    // Get info from the UI parts
+    const dateInput = document.getElementById('search-date').value;
+    const locationInput = document.getElementById('search-location').value;
+    const disasterInput = document.getElementById('search-disaster').value;
     console.log(dateInput);
+
+    // TO DO: get inputs for location bbox and for disaster types
     console.log(locationInput);
     console.log(disasterInput);
-        //sample fetch request, must post 
-        // await fetch('http://localhost:8080/api/test')
-        // .then((response) => response.json())
-        // .then((data) => console.log(data));
-        // await fetch('http://localhost:8080/NASA/env')
-        // .then((response) => response.json())
-        // .then((data) => console.log(data));
+
+    // Make the POST request options
+    const options = {
+        method: 'POST',
+        json: true, 
+        body: JSON.stringify({
+            "start": dateInput,
+            "end": dateInput
+        })
+    };
+
+    // Get the new disaster data
+    fetch('http://localhost:8080/NASA/disasters', options)
+        .then(value =>  value.json())
+        .then(data =>  {
+            console.log(data)
+        });
+   
 }
