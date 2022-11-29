228f95cce24d773dcb6791ce35e9bcc75bb82a44
Sn00pyW00dst0ck <gabealdous@gmail.com>
2022-11-29 17:45:35 -0500

    moving getFromSearch() definition to proper file

--- src/client/js/htmlFuncs.js
+++ src/client/js/htmlFuncs.js
@@ -5,3 +5,19 @@
 window.buttonClicked = function()  {
     console.log("A button was clicked");
 }
+
+window.getFromSearch = function()  {
+    var dateInput = document.getElementById('search-date').value;
+    var locationInput = document.getElementById('search-location').value;
+    var disasterInput = document.getElementById('search-disaster').value;
+    console.log(dateInput);
+    console.log(locationInput);
+    console.log(disasterInput);
+        //sample fetch request, must post 
+        // await fetch('http://localhost:8080/api/test')
+        // .then((response) => response.json())
+        // .then((data) => console.log(data));
+        // await fetch('http://localhost:8080/NASA/env')
+        // .then((response) => response.json())
+        // .then((data) => console.log(data));
+}
