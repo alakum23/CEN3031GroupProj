bb9f94d701eddaaf9a33777b184415590abca1ca
Sn00pyW00dst0ck <gabealdous@gmail.com>
2022-11-29 20:39:30 -0500

    Reliability improvements

--- src/client/js/htmlFuncs.js
+++ src/client/js/htmlFuncs.js
@@ -16,10 +16,13 @@ window.getFromSearch = async function()  {
     // Get coords from bbox
     const toDegrees = (radians) => radians * 180 / Math.PI;
     const bbox = getSelector().rectangle.coordinates._value;
-    const northwest = Rectangle.northwest(bbox);
-	const southeast = Rectangle.southeast(bbox);
-    const boundaryBoxCoord = toDegrees(northwest.longitude) + "," + toDegrees(northwest.latitude) + "," + toDegrees(southeast.longitude) + "," + toDegrees(southeast.latitude);
-
+    let boundaryBoxCoord;
+    if (bbox !== undefined)  {
+        const northwest = Rectangle.northwest(bbox);
+        const southeast = Rectangle.southeast(bbox);
+        boundaryBoxCoord = toDegrees(northwest.longitude) + "," + toDegrees(northwest.latitude) + "," + toDegrees(southeast.longitude) + "," + toDegrees(southeast.latitude);    
+    }
+    
     // Get the disaster name
     const disasterInput = document.getElementById('search-disaster').value;
 
@@ -28,19 +31,18 @@ window.getFromSearch = async function()  {
     console.log(boundaryBoxCoord);
     console.log(disasterInput);
 
-    // Make the POST request options
-    const options = {
-        method: 'POST',
-        json: true, 
-        body: JSON.stringify({
-            "start": dateInput,
-            "end": dateInput,
-            "bbox": boundaryBoxCoord
-        })
-    };
+    // Make the POST request body
+    let bodyObj = {};
+    if (dateInput !== undefined)  { bodyObj["start"] = dateInput; bodyObj["end"] = dateInput;}
+    if (bbox !== undefined)  { bodyObj["bbox"] = boundaryBoxCoord; }
+    if (disasterInput !== undefined)  { bodyObj["categories"] = [disasterInput]; }
 
     // Get the new disaster data
-    fetch('http://localhost:8080/NASA/disasters', options)
+    fetch('http://localhost:8080/NASA/disasters', {
+            method: 'POST',
+            json: true, 
+            body: JSON.stringify(bodyObj)
+        })
         .then(value =>  value.json())
         .then(data =>  {
             console.log(data)
