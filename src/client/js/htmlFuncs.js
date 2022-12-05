964b1475707dca8dccb89775ef46cabe0abc4691
Sn00pyW00dst0ck <gabealdous@gmail.com>
2022-12-05 16:44:53 -0500

    Adding save filters to UI function

--- src/client/js/htmlFuncs.js
+++ src/client/js/htmlFuncs.js
@@ -29,7 +29,60 @@ window.clearLocationSelection = function()  {
     hideSelector();
 }
 
-window.getFromSearch = async function()  {
+window.saveCurrentFilter = async function()  {
+    // Get info from the UI parts
+    const startDateInput = document.getElementById('startDate').value;
+    const endDateInput = document.getElementById('endDate').value;
+
+    // Get coords from bbox
+    let boundaryBoxCoord = undefined;
+    if (getSelector().show === true)  {
+        const bbox = getSelector().rectangle.coordinates._value;
+        if (bbox !== undefined)  {
+            const northwest = Rectangle.northwest(bbox);
+            const southeast = Rectangle.southeast(bbox);
+            const toDegrees = (radians) => radians * 180 / Math.PI;
+            boundaryBoxCoord = toDegrees(northwest.longitude) + "," + toDegrees(northwest.latitude) + "," + toDegrees(southeast.longitude) + "," + toDegrees(southeast.latitude);    
+        }
+    }
+    
+    // Get the disaster types from the checkboxes
+    let disasterCategories = [];
+    ['drought', 'dustHaze', 'earthquakes', 'floods', 'landslides', 'severeStorms', 'snow', 'wildfires', 'volcanoes', 'tempExtremes', 'seaLakeIce', 'waterColor', 'manmade'].forEach((ID) =>  {
+        if (document.getElementById(ID).checked)  {
+            disasterCategories.push(ID);
+        }
+    });
+
+    // Make the POST request body
+    let bodyObj = {};
+    if (startDateInput !== '' && endDateInput !== '')  {
+        bodyObj["start"] = startDateInput;
+        bodyObj["end"] = endDateInput;
+    } else if (startDateInput === '' && endDateInput === '')  {
+    } else  {
+        alert("Provide either both a start and end date or leave both blank");
+        return;
+    }
+    if (boundaryBoxCoord !== undefined)  { bodyObj["boundaryBox"] = boundaryBoxCoord; }
+    if (disasterCategories.length > 0)  { bodyObj["categories"] = disasterCategories; }
+
+    // Print our body to console (for debugging)
+    console.log(bodyObj);
+
+    // Get the new disaster data & add it to the viewer
+    const response = await fetch('http://localhost:8080/mongoose/filters/add', {
+        method: 'PUT',
+        json: true, 
+        body: JSON.stringify(bodyObj)
+    });
+    const responseData = await response.json();
+
+    // Print response in console and add pins to viewer
+    console.log(responseData);
+}
+
+window.applyCurrentFilter = async function()  {
     // Get info from the UI parts
     const startDateInput = document.getElementById('startDate').value;
     const endDateInput = document.getElementById('endDate').value;
